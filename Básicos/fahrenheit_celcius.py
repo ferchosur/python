8c2bd7ce8d1ddcaeb1def3a39a9338c9e53d6b9d
Fernando Giraldo <fernando.gm8@outlook.com>
2020-11-03 21:35:28 -0500

    .

---
+++ Básicos/fahrenheit_celcius.py
@@ -0,0 +1,4 @@
+print("Bienvenido al conversor de grados Fahrenheit a Celsius")
+grados_fahrenheit = float(input("Ingrese los grados Celsius: "))
+grados_celsius = (grados_fahrenheit - 32) * 5/9
+print ("{} grados Fahrenheit equivalen a {} grados Celsius".format(grados_fahrenheit,grados_celsius))
