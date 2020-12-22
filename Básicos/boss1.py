4db2f44d6d7c1b028ecc92471a6e415b9b19c9cc
Fernando Giraldo <fernando.gm8@outlook.com>
2020-12-21 22:05:33 -0500

    final version

--- Básicos/boss1.py
+++ Básicos/boss1.py
@@ -1,72 +1,61 @@
 import random
-print ("Juego de la mazmorra\n"
+print ("Juego del mundo mágico\n"
         "----------------------\n"
         "\n"
-        "Tu y tu compañero de celda os acabaís de escapar de la prisión espacial, habeís burlado a los guardias y os \n"
-        "dirigis hacia el exterior. Entrais en una mazmorra controlada por mounstruos alienigenas, uno de los guardias \n"
-        "ataca a tu compañero, se lo lleva pero tu pasas desapercibido escondido en las sombras. El guardia se retira, \n"
-        "es tu posibilidad de escapar. Hacia dónde te diriges? \n"
+        "Has sido reclutado por un grupo de magos de un mundo paralelo para que les ayudes en su lucha con las fuerzas oscuras que se quieren apoderar \n"
+        "de su mundo. Recién llegas a su mundo, te incluyen en un ritual de iniciación junto a varios nuevos magos reclutados de diferentes mundos. \n"
+        "En el ritual debes elegir cuál va a ser tu poder como mago, te dan 2 opciones para elegir \n"
         "\n"
-        "A la izquierda tienes una puerta semiabierta, a la derecha una escotilla en el suelo. \n")
-opcion = input("[OPCION (A) - Puerta semiabierta] | [OPCION (B) - Escotilla en el suelo]: ")
+        "Puedes tener la habilidad de hacerte invisible en cualquier momento o puedes tener la habilidad de teletransportarte al lugar que imagines \n")
+opcion = input("[OPCION (A) - Invisibilidad] | [OPCION (B) - Teletransportación]: ")
 if opcion == "A":
-    print ("Entras en la puerta semiabierta y otro guardia te descubre, qué haces?")
-    opcion = input ("[OPCION (A) - Te haces el dormido] | [OPCION (B) - Sales corriendo hacia la siguiente puerta]: ")
+    print ("Has elegido la capacidad de hacerte invisible, veamos si eso es de utilidad en este mundo. Sales en compañía de otros dos magos hacía tu primera batalla, \n"
+            "uno de ellos se teletransportó y el otro va volando, así que debes tomar un vehículo, para llegar al lugar donde se librará la batallas, tiens dos opciones, un dragón o bisonte")
+    opcion = input ("[OPCION (A) - Volar en el dragón] | [OPCION (B) - Correr sobre el Bisonte]: ")
     if opcion == "A":
-        print ("El guardia te atrapa, te encierran en una celda de máxima seguridad \n FIN")
+        print ("El dragón no simpatiza contigo y en pleno vuelo te deja caer, mueres antes de iniciar la batalla \n FIN")
     elif opcion == "B":
-        print ("Después de esa puerta encuentras una rana mutante que te regala un puñal casero hecho con la pata de "
-                "una mesa, lo aceptas?")
+        print ("Es un bisonte bastante veloz, llegan justo a tiempo para iniciar la batalla, te encuentra un duende mágico que te ofrece una daga que puede llenar de luz a cuaqluier miembro de las fuerzas oscuras. \n"
+                " Pero usarala tiene un costo muy alto, que solamente será revelado después de usarla, la aceptas?")
         opcion = input("[OPCION (A) - Si] | [OPCION (B)] - No]: ")
         if opcion == "A":
-            print("Coges el pincho y avanzas, hay dos pasillos circulares , no ves el final de ninguno de los dos, uno \n"
-                "está  a la derecha y el otro a la izquierda, cuál tomas?")
-            opcion = input("[OPCION (A) - Izquierda]|[OPCION (B) -  Derecha]")
+            print("Tomas la daga y avanzas hacía la batalla, enfrente hay dos guerreros de las fuerzas oscuras, uno de ellos tiene un gran espada y el otro un arco con felchas \n"
+                "decides hacerte invisible y atacar a uno de ellos, ¿cuál atacas?")
+            opcion = input("[OPCION (A) - El del arco]|[OPCION (B) -  El de la espada]")
             if opcion == "A":
-                print ("La habitación se hace cada vez más oscura, ves tan poco que caes en un agujero en el suelo con \n"
-                        "pinchos, mujeres a las 2 horas de forma lenta y dolorosa. \n FIN")
+                print ("Las flechas de este arco, tienen la capacidad de clavarse en el corazón de cualquiera que esté intentando atacar a su dueño, así que se te clava en el coreazón \n"
+                        "mueres a los 20 minutos de forma lenta y dolorosa. \n FIN")
+                exit()
             elif opcion == "B":
-                print ("Encuentras la salida, en la puerta hay aparcado un Ferrari espacial, te montas, es tu dia de "
-                        "Suerte, las llaves están puestas. Huyes hacia el horizonte")
-        elif opcion = "B"
+                print ("Utilizando tu capacidad de invisibilidad, te ubicas en la espalda de este guerrero y le clavas la daga, lo cual lo llena de luz, tanta luz que termina afectando a su compañero \n"
+                        "ambos mueren, puedes continuar en busca del lider de las fuerzas oscuras")
+        elif opcion == "B":
+            print ("Eres sorprendido por un guerrero de las fuerzas oscuras y dado que no tienes armas para enfrentarlo este te elimina fácilmente \n FIN")
 elif opcion == "B":
-    print("Caes en una zona inundada, el agua te llega hasta las rodillas, avanzas durante media hora y finalmente  "
-            "llegas a una zona abierta, seca y con luz, parecen unas alcantarillas \n\n encuentras un palo largo, parece"
-            "algo pesado, pero podrías servir, lo coges?")
-    opcion = input("[OPCION (A) - Si] | [OPCION (B) - No]: ")
-
-    palo_en_inventario = False
-
+    print("Debes salir hacía la gran batalla junto a dos de tus nuevos compañeros, como te puedes teletransportar, decides llegar lo más pronto al lugar de la batalla. Utilizas una imagen que te mostraron, para imaginar el lugar al que quieres llegar "
+            "llegas a una zona repleta de guerreros de las fuerzas oscuras, al verte rodeado decides saltar a otro lugar, como no conoces más lugares en este mundo, regresas a la base desde la que partiste \n"
+            "sin embargo eres perseguido por miembros de las fuerzas oscuras y al llegar a la base te das cuenta que los debes enfrentar o debes intentar volver a teletransportarte al lugar de la batalla ¿ Qué haces?")
+    opcion = input("[OPCION (A) - Enfrentarlos] | [OPCION (B) - Regresar al sitio de la batalla]: ")
     if opcion == "A":
-        print("Coges el palo")
-        palo_en_inventario = True
-    elif opcion == "B":
-        print ("No coges el palo")
-        palo_en_inventario = False
-    else:
-        print("No has elegido ninguna opcion, simplemente mueres.")
+        print("Por más que luchas, son demasiados y terminas siendo derrotado. \n FIN")
         exit()
-
-    numero_random_rata = random.randint(1, 100)
-    print ("Sigues adelante, encuentras una rata de 2 metros, la rata te pregunta cuanto es 13 * {}).".format(numero_random_rata))
-    opcion = int(input("Cuál es el resultado? "))
-    if opcion == 13 * numero_random_rata:
-        print ("La rata te asesina en el acto, resulta no tolera a los cerebritos. \n FIN")
-    else:
-        print("Mala elección pero buena suerte. La rata abre una pureta delante de ti, parece un pasillo que lleva hasta la salida. Lo recorres, "
-            "llegas al final, el tunel se derrumba detrás de ti, no hay slaida más que una especie  de boca de "
-            "alcantarilla, pero esta lejos de tu alcance, Qué haces?")
-        
-        opcion = input ("[OPCION (A) - Espera a que alguien te rescate] | [OPCION (B) - Intentas salir]")
+    elif opcion == "B":
+        print ("Al regresar logras llegar al mismo lugar donde se encuentran tus compañeros de batalla, ellos ya han eliminado algunos rivales, mientras vas caminando un duende mágico te ofrece una semilla que puede convertirse en cualquier arma que imagines, pero mientras utilizas esta arma no puedes teletransportarte ¿ la tomas?")
+        opcion = input("[OPCION (A) - SI] | [OPCION (B) - NO]: ")
+        semilla_en_inventario = False
         if opcion == "A":
-            print ("Un monton de ratas aparecen y te devoran vivo, es tu fin\nFIN")
-        elif opcion == "B" and palo_en_inventario == True:
-            print ("Usas el palo que cogiste antes para impulsarte, consigues trepar y salir del laberinto. En la"
-                    " puerta hay aparcado un ferrari espacial, te montas, es tu día de suerte, las llaves están puestas, "
-                    "Huyes hacia el horizonte\nFIN")
-        elif opcion == "B" and palo_en_inventario == False:
-            print ("No tienes como subir, si solo tuvieras un palo... Pelo no lo tienes verdad? Así que finalmente te "
-                    "quedas atrapado. \n\n Pasado un rato un monton de ratas aparecen y te devoranvivo. Es tu fin \n FIN")
+            print("Coges la semilla")
+            semilla_en_inventario = True
+        elif opcion == "B":
+            print ("No coges la semilla")
+            semilla_en_inventario = False
+        print ("Sigues adelante, encuentras una línea de defensa del ejercito oscuro ¿Estás decidido a enfrentarlos?")
+        opcion = input("[OPCION (A) - SI] | [OPCION (B) - NO]: ")
+        if opcion == "A" and semilla_en_inventario :
+            print ("Utilizas la magia de la semilla e imaginando una cadena de andromeda con 1000 puntas letales. Puedes continuar en busca del lider de las fuerzas oscuras \n FIN")
+        else:
+            print("Mala elección no logras huir, son demasiados guerreros, te capturan y te eliminan \n FIN")
+
 else:
     print("No has elegido ninguna opción. Simplemente mueres")
 
