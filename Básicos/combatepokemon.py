7a420d8c00a535ea6c9f5b66109230cb9f9d7a85
Fernando Giraldo <fernando.gm8@outlook.com>
2020-12-22 20:52:44 -0500

    .

--- Básicos/combatepokemon.py
+++ Básicos/combatepokemon.py
@@ -1,4 +1,6 @@
 from random import randint
+import os
+
 
 vida_inicial_pikachu = 80
 vida_inicial_squirtle = 90
@@ -24,17 +26,23 @@ while vida_squirtle>0 and vida_pikachu > 0:
     barras_de_vida_pikachu = int(vida_pikachu * 10 / vida_inicial_pikachu)
     print ("Pikachu:      [{}{}] ({}/{})".format("*" * barras_de_vida_pikachu, " " * (10 - barras_de_vida_pikachu),vida_pikachu, vida_inicial_pikachu))
 
+    if vida_squirtle < 0:
+        vida_squirtle = 0
+    if vida_pikachu < 0:
+        vida_pikachu =0
+
     barras_de_vida_squirtle = int(vida_squirtle * 10 / vida_inicial_squirtle)
     print ("Squirtle:     [{}{}] ({}/{})".format("*" * barras_de_vida_squirtle, " " * (10 - barras_de_vida_squirtle), vida_squirtle, vida_inicial_squirtle))
 
     input (" Enter para continuar... \n\n")
+    os.system("clear")
 
     #Turno para Squirtle
     print ("Turno Squirtle")
 
     ataque_squirtle = None
     while ataque_squirtle != "P" and ataque_squirtle != "A" and ataque_squirtle !="B":
-        ataque_squirtle = input("¿Qué ataque deseas realizar? [P]lacaje. Pistola [A]gua, [B]urbuja: ")
+        ataque_squirtle = input("¿Qué ataque deseas realizar? [P]lacaje. Pistola [A]gua, [B]urbuja, [N]ada: ")
 
     if ataque_squirtle == "P":
         print("Squirtle ataca con placaje")
@@ -45,6 +53,13 @@ while vida_squirtle>0 and vida_pikachu > 0:
     elif ataque_squirtle == "B":
         print("Squirtle ataca con Burbuja")
         vida_pikachu -= 9
+    elif ataque_squirtle == "N":
+        print("Squirtle no hace nada")
+
+    if vida_squirtle < 0:
+        vida_squirtle = 0
+    if vida_pikachu < 0:
+        vida_pikachu =0 
 
     barras_de_vida_pikachu = int(vida_pikachu * 10 / vida_inicial_pikachu)
     print ("Pikachu:      [{}{}] ({}/{})".format("*" * barras_de_vida_pikachu, " " * (10 - barras_de_vida_pikachu),vida_pikachu, vida_inicial_pikachu))
@@ -53,7 +68,8 @@ while vida_squirtle>0 and vida_pikachu > 0:
     print ("Squirtle:     [{}{}] ({}/{})".format("*" * barras_de_vida_squirtle, " " * (10 - barras_de_vida_squirtle), vida_squirtle, vida_inicial_squirtle))
 
     input("Enter para continuar... \n\n")
-
+    os.system("clear")
+    
 if vida_pikachu > vida_squirtle:
     print("Pikachu gana!")
 else:
